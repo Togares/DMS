826623cf1da5e4e03b55414a9ac1876e657f655f
Togares <71195452+Togares@users.noreply.github.com>
2021-06-22 09:24:00 +0200

    CommonTypes hinzugefügt
    File und Drive hinzugefügt
    nicht benutzte using direktiven entfernt

---
+++ DMS/CommonTypes/Drive.cs
@@ -0,0 +1,20 @@
+﻿using CommonTypes.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonTypes
+{
+    public class Drive : Bindable
+    {
+		private string _Name;
+
+		public string Name
+		{
+			get { return _Name; }
+			set { _Name = value; OnPropertyChanged(); }
+		}
+	}
+}
