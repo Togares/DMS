b467574770481508906e33d49264d2fda6529119
Togares <p-renz@outlook.com>
2021-06-22 13:37:42 +0200

    RelayCommand
    ICommand Implementierung hinzugefügt

---
+++ DMS/CommonTypes/Utility/RelayCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace CommonTypes.Utility
+{
+    public class RelayCommand<T> : ICommand
+    {
+        private Action<T> _Execute = null;
+        private Predicate<T> _CanExecute = null;
+
+        public RelayCommand(Action<T> execute, Predicate<T> canExecute = null)
+        {
+            _Execute = execute;
+            _CanExecute = canExecute;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)
+        {
+            _Execute?.Invoke((T)parameter);
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return (bool)_CanExecute?.Invoke((T)parameter);
+        }
+
+    }
+}
