b2050d4b5485757ce2d4db1451d9eb08c2f35ab0
begli.amanov <begli.amanov@outlook.com>
2021-06-29 00:04:37 +0200

    Add Base UI (but not relevant to the project)

---
+++ DMS/DMS/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,43 @@
+﻿using CommonTypes.Utility;
+
+namespace DMS.MVVM.ViewModel
+{
+    class MainViewModel : Bindable
+    {
+
+        public RelayCommand HomeViewCommand { get; set; }
+        public RelayCommand DiscoveryViewCommand { get; set; }
+
+        public HomeViewModel HomeVM { get; set; }
+        public DiscoveryViewModel DicoveryVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get => _currentView;
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            DicoveryVM = new DiscoveryViewModel();
+            CurrentView = HomeVM;
+
+            HomeViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = HomeVM;
+            });
+
+            DiscoveryViewCommand = new RelayCommand(o =>
+            {
+                CurrentView = DicoveryVM;
+            });
+        }
+    }
+}
