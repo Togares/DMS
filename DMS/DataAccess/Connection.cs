adc5ad0d92b53e121996cb0151adb8da0eba5849
Togares <71195452+Togares@users.noreply.github.com>
2021-06-29 11:33:55 +0200

    Connection
    test connection auf schuldb server

---
+++ DMS/DataAccess/Connection.cs
@@ -0,0 +1,31 @@
+﻿using Npgsql;
+
+namespace DataAccess
+{
+    public class Connection
+    {
+        private NpgsqlConnection _Connection;
+
+        public Connection()
+        {
+            _Connection = new NpgsqlConnection();
+        }
+
+        public void Connect(string user, string password, string host, int port, string database)
+        {
+            NpgsqlConnectionStringBuilder builder = new NpgsqlConnectionStringBuilder();
+            builder.Username = user;
+            builder.Password = password;
+            builder.Database = database;
+            builder.Port = port;
+            builder.Host = host;
+            _Connection.ConnectionString = builder.ConnectionString;
+            _Connection.Open();
+        }
+
+        public void Disconnect()
+        {
+            _Connection.Close();
+        }
+    }
+}
