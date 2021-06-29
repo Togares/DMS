84ac76051eaec27e4cc2dae87e9ec57771e57be3
Togares <71195452+Togares@users.noreply.github.com>
2021-06-29 09:10:42 +0200

    Properties hinzugefügt

--- DMS/CommonTypes/File.cs
+++ DMS/CommonTypes/File.cs
@@ -1,17 +1,46 @@
 ﻿using CommonTypes.Utility;
+using System;
 
 namespace CommonTypes
 {
     public class File : Bindable
     {
-        private string _Path;
-
-        public string Path
+        public File()
         {
-            get { return _Path; }
-            set { _Path = value; OnPropertyChanged(); }
+
         }
 
+        private long _ID;
+
+        public long ID
+        {
+            get { return _ID; }
+            set { _ID = value; OnPropertyChanged(); }
+        }
+
+        private byte[] _Content;
+
+        public byte[] Content
+        {
+            get { return _Content; }
+            set { _Content = value; OnPropertyChanged(); }
+        }
+
+        private DateTime _Created;
+
+        public DateTime Created
+        {
+            get { return _Created; }
+            set { _Created = value; OnPropertyChanged(); }
+        }
+        
+        private DateTime _Modified;
+
+        public DateTime Modified
+        {
+            get { return _Modified; }
+            set { _Modified = value; OnPropertyChanged(); }
+        }
 
         private string _Name;
 
@@ -21,18 +50,31 @@ namespace CommonTypes
             set { _Name = value; OnPropertyChanged(); }
         }
 
-        private int _Size;
+        private string _Path;
+
+        public string Path
+        {
+            get { return _Path; }
+            set { _Path = value; OnPropertyChanged(); }
+        }
+
+        private string _Type;
+
+        public string Type
+        {
+            get { return _Type; }
+            set { _Type = value; OnPropertyChanged(); }
+        }
+
 
         /// <summary>
         /// Bytes
         /// </summary>
         public int Size
         {
-            get { return _Size; }
-            set { _Size = value; OnPropertyChanged(); }
+            get { return _Content.Length; }
         }
 
-        // inhalt
-        // 
+
     }
 }
