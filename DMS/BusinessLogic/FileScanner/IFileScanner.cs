5a3145d6b735eae9c8be0d7be81ad4f6143a8b6c
Paul Renz <renz-p@web.de>
2021-07-02 23:43:51 +0200

    Scanner für Dateien Testprojekt für BusinessLogic, Tests für Dateiscanner

---
+++ DMS/BusinessLogic/FileScanner/IFileScanner.cs
@@ -0,0 +1,21 @@
+﻿using CommonTypes;
+using System.Collections.Generic;
+using BusinessLogic.FileScanner.Events;
+
+namespace BusinessLogic.FileScanner
+{
+    namespace Events
+    {
+        public delegate void DriveScanFinishedEventHandler(IEnumerable<Drive> drives);
+        public delegate void FileScanFinishedEventHandler(IEnumerable<File> files);
+    }
+
+    public interface IFileScanner
+    {
+        event DriveScanFinishedEventHandler DriveScanFinished;
+        event FileScanFinishedEventHandler FileScanFinished;
+
+        void ScanDrives();
+        void ScanDirectory(string path, bool recursive);
+    }
+}
