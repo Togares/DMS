ffc4df37c88591d75adc14b75649eaa1e2cd8248
Paul Renz <p-renz@outlook.com>
2021-10-01 08:02:39 +0200

    Suche angepasst
    - wird im Suchfeld nichts eingegeben, und anschließend gesucht, werden alle Dateien angezeigt
    - Hand-Cursor im HamburgerMenu eingefügt (Hover)

--- DMS/DataAccess/Connection.cs
+++ DMS/DataAccess/Connection.cs
@@ -28,7 +28,7 @@ namespace DataAccess
 
         private Connection()
         {
-            
+
         }
 
         public static Connection Get()
@@ -122,7 +122,14 @@ namespace DataAccess
             IEnumerable<File> result = null;
             using (FileContext context = new FileContext(Get(), false))
             {
-                result = context.Files.Where(x => NpgsqlTextFunctions.Match(NpgsqlTextFunctions.ToTsVector(x.Content), NpgsqlTextFunctions.PlainToTsQuery(filter))).ToList();
+                if (string.IsNullOrEmpty(filter))
+                {
+                    result = context.Files.ToList();
+                }
+                else
+                {
+                    result = context.Files.Where(x => NpgsqlTextFunctions.Match(NpgsqlTextFunctions.ToTsVector(x.Content), NpgsqlTextFunctions.PlainToTsQuery(filter))).ToList();
+                }
             }
             return result;
         }
